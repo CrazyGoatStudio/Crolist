3ec807ade4138af0d71eaa9cad7a5fb777f05d41
EndermanAPM <xavier1997@gmail.com>
2017-03-07 00:08:47 +0100

    route changes

--- build_apk.sh
+++ build_apk.sh
@@ -1,5 +1,4 @@
-cd Crolist/
-- echo "Script directory" && echo pwd
+echo "Script directory" && pwd
 cordova plugin rm cordova-plugin-console
 cordova platform add android
 cordova build --release android
