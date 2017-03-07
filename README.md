b5a0f1db929f07529618458431d89f41d09d8aa5
EndermanAPM <xavier1997@gmail.com>
2017-03-07 02:38:25 +0100

    Add build status makdown and deploy to github releases

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Crolist
+[![Build Status](https://travis-ci.org/CrazyGoatStudio/Crolist.svg?branch=master)](https://travis-ci.org/CrazyGoatStudio/Crolist)
--- build_apk.sh
+++
@@ -1,7 +0,0 @@
-echo "Script directory" && pwd
-mkdir www
-mkdir "$ANDROID_HOME/licenses"
-echo -e "\n8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_HOME/licenses/android-sdk-license"
-cordova plugin rm cordova-plugin-console
-cordova platform add android
-cordova build --release android
